d97ac41c8d2262579db7ea3aed70f01caab89e76
Cheyiibrahim <oluwaseyiibrahim357@gmail.com>
2024-01-02 19:01:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let Yourname = prompt("Enter your name please") ;
+
+let firstLetter = Yourname.charAt(0).toUpperCase();
+
+let restLetters = Yourname.slice(1).toLowerCase() ;
+
+let final = alert ( " hello " + firstLetter + restLetters + " you are welcome ") ;
